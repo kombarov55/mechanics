297eae116f92decceb8085692827e5089124592d
Nikolay <kombarov55@gmail.com>
2018-04-18 19:01:46 +0300

    added cors

--- src/main/kotlin/com/company/demo/DemoApplication.kt
+++ src/main/kotlin/com/company/demo/DemoApplication.kt
@@ -3,11 +3,13 @@ package com.company.demo
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
 
 var dbHost = "localhost"
 var dbPort = 27017
 
-@SpringBootApplication()
+@SpringBootApplication
+@CrossOrigin
 @EnableAutoConfiguration
 class DemoApplication
 
